1f9befa5bbad10f6c1db9844ffbe3102f2e3f8d2
Visit029MDT <112145096+MDTVisit@users.noreply.github.com>
2023-04-30 16:06:44 +0700

    Test_1

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.Write("Enter number of nodes: ");
+    int n = int.Parse(Console.ReadLine());
+
+    int[,] graph = new int[n, n];
+
+    Console.WriteLine("Enter edges (from node i to node j):");
+    int i, j;
+    while (true) {
+      Console.Write("i = ");
+      i = int.Parse(Console.ReadLine());
+      if (i < 0 || i >= n) break;
+
+      Console.Write("j = ");
+      j = int.Parse(Console.ReadLine());
+      if (j < 0 || j >= n) break;
+
+      graph[i, j] = 1;
+    }
+
+    Console.WriteLine("Enter nodes to check (from node j to node i):");
+    while (true) {
+      Console.Write("j check = ");
+      j = int.Parse(Console.ReadLine());
+      if (j < 0 || j >= n) break;
+
+      Console.Write("i check = ");
+      i = int.Parse(Console.ReadLine());
+      if (i < 0 || i >= n) break;
+
+      bool reachable = IsReachable(graph, j, i, n);
+      if (reachable) {
+        Console.WriteLine("Reachable");
+      } else {
+        Console.WriteLine("Unreachable");
+      }
+    }
+  }
+
+  static bool IsReachable(int[,] graph, int j, int i, int n) {
+    bool[] visited = new bool[n];
+    visited[j] = true;
+    return DFS(graph, visited, j, i);
+  }
+
+  static bool DFS(int[,] graph, bool[] visited, int j, int i) {
+    if (j == i) {
+      return true;
+    }
+
+    for (int k = 0; k < visited.Length; k++) {
+      if (graph[j, k] == 1 && !visited[k]) {
+        visited[k] = true;
+        if (DFS(graph, visited, k, i)) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+}
